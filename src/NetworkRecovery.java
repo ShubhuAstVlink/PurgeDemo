6986e69ff450b70ccee52105b4533700d7c1e291
Shubham Asthana <shubham.asthana@vlinkinfo.com>
2026-08-25 18:47:20 +0530

    code update

--- src/NetworkRecovery.java
+++ src/NetworkRecovery.java
@@ -6,7 +6,7 @@ public class NetworkRecovery {
     public int findMaxPathScore(int[][] edges, boolean[] online, long k) {
 
         PriorityQueue<int[]> queue = new PriorityQueue<>();
-
-        return -1;
+        queue.offer(new int[] {0, 0});
+        return 1;
     }
 }
